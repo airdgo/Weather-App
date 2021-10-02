3142370b57b4777897938138660c3accdf4fb044
airdgo <vladdragoi1@gmail.com>
2021-10-02 12:49:40 +0300

    hidden apikey

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+import APIKEY from "./apikey.js";
+
 const searchInput = document.getElementById("search");
 const form = document.getElementById("form");
 
-const APIKEY = "624a1045c7d6dfb1ff081f2e59156af2";
+// const APIKEY = "624a1045c7d6dfb1ff081f2e59156af2";
 const APIURL = (location) =>
 	`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKEY}`;
 
 async function getWeatherByLocation(location) {
 	const response = await fetch(APIURL(location));
 	const responseData = await response.json();
-	console.log(responseData, KtoC(responseData.main.temp));
+	console.log(
+		responseData,
+		KtoC(responseData.main.temp),
+		responseData.coord.lon,
+		responseData.coord.lat
+	);
 }
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
