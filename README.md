8c6863915df13b0751e6b5edf224d35025bb8263
airdgo <vladdragoi1@gmail.com>
2021-10-02 11:24:25 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Weather App
+
+This is a simple weather app in wich users can search by location.
---
+++ script.js
@@ -0,0 +1,18 @@
+const searchInput = document.getElementById("search");
+const form = document.getElementById("form");
+
+const APIKEY = "624a1045c7d6dfb1ff081f2e59156af2";
+const APIURL = (location) =>
+	`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKEY}`;
+
+async function getWeatherByLocation(location) {
+	const response = await fetch(APIURL(location));
+	const responseData = await response.json();
+	console.log(responseData, KtoC(responseData.main.temp));
+}
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	getWeatherByLocation(searchInput.value);
+});
+
+const KtoC = (temp) => Math.floor(temp - 273.15);
