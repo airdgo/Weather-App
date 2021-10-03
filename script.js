d8818f0670e08f1aa76da79c3cd483cd7469d7e1
airdgo <vladdragoi1@gmail.com>
2021-10-03 12:54:15 +0300

    styling

--- script.js
+++ script.js
@@ -2,24 +2,26 @@ import APIKEY from "./node_modules/apikey.js";
 
 const searchInput = document.getElementById("search");
 const form = document.getElementById("form");
+const weather = document.getElementById("weather");
 
-// const APIKEY = "624a1045c7d6dfb1ff081f2e59156af2";
-const APIURL = (location) =>
+const location_APIURL = (location) =>
 	`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKEY}`;
 
+const weather_APIURL = (latitude, longitude) =>
+	`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&exclude=minutely&appid=${APIKEY}`;
+
 async function getWeatherByLocation(location) {
-	const response = await fetch(APIURL(location));
+	const response = await fetch(location_APIURL(location));
 	const responseData = await response.json();
-	console.log(
-		responseData,
-		KtoC(responseData.main.temp),
-		responseData.coord.lon,
-		responseData.coord.lat
+	console.log(responseData);
+	const resp = await fetch(
+		weather_APIURL(responseData.coord.lat, responseData.coord.lon)
 	);
+	const respData = await resp.json();
+	console.log(respData);
 }
+
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	getWeatherByLocation(searchInput.value);
 });
-
-const KtoC = (temp) => Math.floor(temp - 273.15);
